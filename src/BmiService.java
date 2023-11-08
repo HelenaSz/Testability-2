fced568141457f2c3d7b6178404118739b45522b
Neto work <lenas_2003@mail.ru>
2023-11-09 01:43:18 +0200

    Refactoring code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,4 @@
 public class BmiService {
-
     public double calculate(double heightInMeters, double weightInKilograms) {
         double index;
         index = weightInKilograms / (heightInMeters * heightInMeters);
