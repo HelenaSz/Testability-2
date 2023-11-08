ec648adfdf2af9bc2711b02a8d232a67c3183c2b
Neto work <lenas_2003@mail.ru>
2023-11-09 01:05:17 +0200

    Refactoring code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double height_in_meters = 1.87;
-        double weight_in_kilograms = 98;
-        double index = service.calculate(height_in_meters, weight_in_kilograms);
+        double heightInMeters = 1.87;
+        double weightInKilograms = 98;
+        double index = service.calculate(heightInMeters, weightInKilograms);
         System.out.println(index);
     }
 }
